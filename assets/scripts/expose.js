8a33b3791e30af7a91bfd2f5cdeed6a44a2ef1ce
YueSteveYin <yuy045@ucsd.edu>
2023-05-07 18:15:09 -0700

    final change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,39 +1,42 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
   // TODO
   //horn selection
+  let jsConfetti = new JSConfetti();
   let hornSelection = document.getElementById("horn-select");
-  let hornSound = document.getElementsByClassName("hidden");
+  let hornsound = document.querySelector("audio");
   let hornImage = document.querySelector("img");
   hornSelection.addEventListener("change", (Event) => {
-    hornSound.src = "/assets/audio/ ${Event.target.value} .mp3";
-    hornImage.src = "/assets/images/ ${Event.target.value} .svg";
+    hornsound.src = "/assets/audio/" + Event.target.value + ".mp3";
+    hornImage.src = "/assets/images/" + Event.target.value + ".svg";
+    if(Event.target.value == "party-horn"){
+      jsConfetti.addConfetti();
+    }
   });
   //Sound playing
   let playPressed = document.querySelector("button");
-  let sound = document.getElementsByClassName("hidden");
   playPressed.addEventListener("click", (Event) => {
-    sound.play();
+    hornsound.play();
   });
   //sound level
   let volumeControl = document.getElementById("volume");
   let volumeImage = document.querySelector("div#volume-controls img");
   volumeControl.addEventListener("input", (Event) => {
-    if(Event.value == 0){
-      volumeImage.src = "/assets/icon/volume-level-0.svg";
-      sound.volume = 0.25;
-    }else if(Event.value < 33){
-      volumeImage.src = "/assets/icon/volume-level-1.svg";
-      sound.volume = 0.5;
-    }else if(Event.value < 67){
-      volumeImage.src = "/assets/icon/volume-level-2.svg";
-      sound.volume = 0.75;
-    }else{
-      volumeImage.src = "/assets/icon/volume-level-3.svg";
-      sound.volume = 1.0;
+    console.log(volumeControl.value);
+    if(volumeControl.value == 0){
+      volumeImage.src = "/assets/icons/volume-level-0.svg";
+      hornsound.volume = 0;
+    }else if(volumeControl.value < 33){
+      volumeImage.src = "/assets/icons/volume-level-1.svg";
+      hornsound.volume = 0.33;
+    }else if(volumeControl.value < 67){
+      volumeImage.src = "/assets/icons/volume-level-2.svg";
+      hornsound.volume = 0.66;
+    }else if(volumeControl.value > 66){
+      volumeImage.src = "/assets/icons/volume-level-3.svg";
+      hornsound.volume = 1.0;
     }
   });
 }
